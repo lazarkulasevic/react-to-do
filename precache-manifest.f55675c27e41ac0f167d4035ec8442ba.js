e8c7242fba8be44936b1beb5070b893df051a4c9
lazarkulasevic <lazar.kulasevic@gmail.com>
2020-10-03 23:44:46 +0200

    Updates

---
+++ precache-manifest.f55675c27e41ac0f167d4035ec8442ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1514dd5313fda23abe35ace4b3aaa7",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "82e55a4e26cefd2a4bc1",
+    "url": "/react-to-do/static/css/main.5a027c99.chunk.css"
+  },
+  {
+    "revision": "05fd4a0ec34deeff73a8",
+    "url": "/react-to-do/static/js/2.4fbc345a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do/static/js/2.4fbc345a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e55a4e26cefd2a4bc1",
+    "url": "/react-to-do/static/js/main.1ad441d1.chunk.js"
+  },
+  {
+    "revision": "d7b3e816c749cef8f96e",
+    "url": "/react-to-do/static/js/runtime-main.2169f813.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-do/static/media/logo.5d5d9eef.svg"
+  }
+]);
